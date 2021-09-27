6ff924cff21026ce1e827c4afe1521b3f5d27ddb
tor <sithipol1@hotmail.com>
2021-09-28 01:16:34 +0700

    tor:set basic file

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ import Express from "express";
 
 const app = Express()
 const port = 3000
+
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }))
 
-app.get('/:id', (req, res) => {
-  res.json({ "sss": req.params.id })
+app.use(function (req, res, next) {
+  // const allowedOrigins = ['http://localhost:8080'];
+  // const origin = req.headers.origin;
+  // if (allowedOrigins.includes(origin)) {
+  //   res.setHeader('Access-Control-Allow-Origin', origin);
+  // }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({ "message": "hello!" })
 })
 
 
